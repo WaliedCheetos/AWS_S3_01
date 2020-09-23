e0a6c2ce22e5b9c05985302a0370f50d4cd98d69
WaliedCheetos <Walied.Cheetos@gmail.com>
2020-09-23 22:58:10 +0200

    03

--- js/geolocation.js
+++ js/geolocation.js
@@ -10,9 +10,30 @@ function getLocation() {
 function getCurrentPosition_successCB(successCB) {
     latlng = "getCurrentPosition_successCB: " + "Latitude: " + successCB.coords.latitude + "<br>Longitude: " + successCB.coords.longitude;
     console.log(latlng);
+    geocodeHERE(successCB.coords.latitude + ',' + successCB.coords.longitude);
 }
 
 function getCurrentPosition_errorCB(errorCB) {
     errorMsg = "getCurrentPosition_successCB: " + errorCB;
-    console.log(errorMsg);
+    console.error(errorMsg);
 }
+
+async function geocodeHERE(latlng) {
+    geocodeURLString = 'https://revgeocode.search.hereapi.com/v1/revgeocode?apikey=zqXX556h4qRUgZyA-PtL9CgTohrfxmFIwVSugJwou9w&lang=en-US&at=' + latlng
+    $.ajax({
+        url: geocodeURLString,
+        type: 'GET',
+        success: function (successCB) {
+            console.log(JSON.stringify(successCB));
+        },
+        error: function (errorCB) {
+            console.error(JSON.stringify(errorCB));
+        }
+    });
+
+    //let response = await fetch('https://revgeocode.search.hereapi.com/v1/revgeocode?apikey=zqXX556h4qRUgZyA-PtL9CgTohrfxmFIwVSugJwou9w&lang=en-US&at=' + latlng);
+    //let data = await response.json();
+    //return data;
+}
+
+//geocodeHERE.then(data => console.log(data));
